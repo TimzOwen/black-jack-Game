7dd0a70dd57e01d45bb47dceb6cbe4d0559d59c9
Timz Owen <34624703+TimzOwen@users.noreply.github.com>
2019-01-21 06:09:07 -0500

    Add files via upload
    picker vs a dealer script gamer

---
+++ javascript.js
@@ -0,0 +1,13 @@
+console.log("amin No.")
+alert("i the file attached?")
+//create a simplee API to fork from M-PESA
+function promptUser()
+{
+  var texty;
+  var userName = window.prompt("nter user name");
+  if(userName != null || userName !="")
+  {
+    document.getElementById("userName").innerHTML = userName;
+  }
+  texty = "hello" + userName;
+}
