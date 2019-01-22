4d58126e4281193c72e03f51fce37fa1d230ad4d
TimzOwen <timzowen1130@gmail.com>
2019-01-22 12:07:38 -0500

    a simplified of black-jack js game

---
+++ cardShuffler.js
@@ -0,0 +1,12 @@
+//black-jack game by
+//Owen Timz
+//create variable and output
+
+let card1 = 'Ace of Spade'
+    card2 = 'Ten of Hearts';
+//print Results
+console.log("Welcome to Black-Jack Game");
+
+console.log("you are dealt: ");
+console.log(" " + card1);
+console.log( '' + card2);
