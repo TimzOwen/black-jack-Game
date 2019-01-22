f48aa99bc4387dc4b4e070630164b35c059f7946
TimzOwen <timzowen1130@gmail.com>
2019-01-22 12:50:43 -0500

    added for loopps to generate cards

--- cardShuffler.js
+++ cardShuffler.js
@@ -2,10 +2,30 @@
 //Owen Timz
 //create variable and output
 
+//loop between arrays
+let suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades'];
+
+let values = ['Ace', 'King', 'Queen', 'Jack', 'Ten', 'Nine',
+  'Nine', 'Eight', 'Seven', 'Six', 'Five', 'Four',
+  'Three', 'Two', 'One'
+];
+
 //create arrays to hold the cards
-let deck = ['Ace of Spade',
-            'Two of Spade',
-            'Three of Spade'];
+let deck = [];
+//use for lop to generate the cards and store in deck[];
+for (let valueIdx = 0; valueIdx < values.length; valueIdx++)
+{
+  for(suitIdx = 0; suitIdx < suits.length; suitIdx++)
+  {
+    //push to the createDeck
+    deck.push(values[valueIdx] + ' of ' + suits[suitIdx]);
+  }
+}
+//check if the cards are created;
+for(let i = 0; i < deck.length; i++)
+{
+  console.log(deck[i]);
+}
 //variable to hold players getCardString
 let playerCards = [deck[0], deck[1]];
 //print Results
